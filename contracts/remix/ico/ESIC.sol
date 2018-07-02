df2fef0f1aafd45573753d37a94f85870f67a527
oldcpu <oldcpu@163.com>
2018-07-03 02:30:19 +0800

    Update ESIC.sol
    Update the symbol "ET"

--- contracts/remix/ico/ESIC.sol
+++ contracts/remix/ico/ESIC.sol
@@ -5,7 +5,7 @@ import 'github.com/OpenZeppelin/zeppelin-solidity/contracts/token/ERC20/Standard
 
 contract ESICToken is StandardToken {
     string public name = "Enterprise Service Improvement Chain";
-    string public symbol = "ESIC";
+    string public symbol = "ET";
     uint8 public decimals = 18;
     uint256 public INITIAL_SUPPLY = 10000000000000000000000000000;
 
